c732767819370b9cfe9bf6c6a5640adc701667a5
netcan <1469709759@qq.com>
2016-08-17 20:13:55 +0800

    add fork on github

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,9 +9,11 @@
         <script src="http://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <script src="http://cdn.bootcss.com/toastr.js/2.1.3/toastr.min.js"></script>
         <script src="{{ asset('js/app.js') }}"></script>
+        <style>#forkongithub a{background:#000;color:#fff;text-decoration:none;font-family:arial,sans-serif;text-align:center;font-weight:bold;padding:5px 40px;font-size:1rem;line-height:2rem;position:relative;transition:0.5s;}#forkongithub a:hover{background:#c11;color:#fff;}#forkongithub a::before,#forkongithub a::after{content:"";width:100%;display:block;position:absolute;top:1px;left:0;height:1px;background:#fff;}#forkongithub a::after{bottom:1px;top:auto;}@media screen and (min-width:800px){#forkongithub{position:fixed;display:block;top:0;right:0;width:200px;overflow:hidden;height:200px;z-index:9999;}#forkongithub a{width:200px;position:absolute;top:42px;right:-42px;transform:rotate(45deg);-webkit-transform:rotate(45deg);-ms-transform:rotate(45deg);-moz-transform:rotate(45deg);-o-transform:rotate(45deg);box-shadow:4px 4px 10px rgba(0,0,0,0.8);}}</style><span id="forkongithub">
     </head>
     <body>
-        <div class="container">
+    <a href="https://github.com/netcan/Laravel_AJAX_CRUD" target="_blank">Fork me on GitHub</a></span>
+    <div class="container">
             {{--task list--}}
             <div class="panel panel-default">
                 <div class="panel-heading">
